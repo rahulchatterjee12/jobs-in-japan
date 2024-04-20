8cecda6dca5da5be8b756a570de865990a864872
rc5292684 <rc5292684@gmail.com>
2024-04-20 12:24:08 +0530

    font and header changes

--- src/components/JobSearch.js
+++ src/components/JobSearch.js
@@ -2,7 +2,7 @@ import styles from "./JobSearch.module.css";
 
 function JobSearch() {
   return (
-    <div className="text-center w-[100vw]">
+    <div className="text-center w-[100vw] mt-10">
       <h2 className={styles.title}>Search for Jobs</h2>
       <form className={styles.searchForm}>
         <input
